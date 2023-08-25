86762cac9ef371edf7ab7e9d34eed3f2d2a3f998
nht198 <nguyenhuytrung198@gmail.com>
2023-08-25 22:18:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+//Pages
+import {Home, Contact} from './pages/index';
+//Components
+import {Header, Footer} from './components/index';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
