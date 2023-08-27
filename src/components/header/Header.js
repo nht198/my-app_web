40d817d485fd5350cc42489d9fdb0ff75189e213
nht198 <nguyenhuytrung198@gmail.com>
2023-08-27 08:00:00 +0000

    update 26-Aug-2023

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, NavLink } from "react-router-dom"
+import styles from "./Header.module.scss"
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>
+      </h2>
+    </Link>
+  </div>
+)
+
+const activeLink = ({isActive}) => (isActive?`${styles.active}`:"");
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMeu = () => setShowMenu(!showMenu);
+  const hideMenu = () => setShowMenu(false)
+
   return (
-    <div>Header</div>
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav className={showMenu?`${styles["show-nav"]}`:`${styles["hide-nav"]}`}>
+          <div className={showMenu
+            ?`${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+            :`${styles["nav-wrapper"]}`}
+            onClick={hideMenu}>    
+          </div>
+        </nav>
+      </div>
+    </header>
   )
 }
 
