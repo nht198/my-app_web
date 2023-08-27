ce5896c3cda53bf3238866c1e8899154c59822bc
nht198 <nguyenhuytrung198@gmail.com>
2023-08-27 16:06:21 +0700

    update 27-Aug-2023

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
-import { Link, NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import styles from "./Header.module.scss"
+import {HiOutlineMenuAlt3} from "react-icons/hi"
 
 const logo = (
   <div className={styles.logo}>
@@ -16,7 +17,7 @@ const activeLink = ({isActive}) => (isActive?`${styles.active}`:"");
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const toggleMeu = () => setShowMenu(!showMenu);
+  const toggleMenu = () => setShowMenu(!showMenu);
   const hideMenu = () => setShowMenu(false)
 
   return (
@@ -29,7 +30,18 @@ const Header = () => {
             :`${styles["nav-wrapper"]}`}
             onClick={hideMenu}>    
           </div>
+          <ul onClick={hideMenu}>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
         </nav>
+        <div className={styles["menu-icon"]}>
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
       </div>
     </header>
   )
